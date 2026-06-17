a8f34b82276022d576cd6dc2fb067e372cb69f68
jhjeon81 <juhongjeon81@gmail.com>
2026-06-17 10:35:02 +0900

    fix: main.jsx 내부 호출 경로 대문자로 최종 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { VehicleProvider } from './context/vehicleContext.jsx'
+import { VehicleProvider } from './context/VehicleContext.jsx'
 import { PreRegisterProvider } from './context/PreRegisterContext.jsx'
 
 createRoot(document.getElementById('root')).render(
